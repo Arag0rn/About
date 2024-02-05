b4e882647d32866b25168532ee610adbe08f7b81
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-02-05 23:57:41 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
-const createNextIntlPlugin = require('next-intl/plugin');
-const withNextIntl = createNextIntlPlugin();
-
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  // output: 'export',
-};
-
-module.exports = withNextIntl(nextConfig);
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
